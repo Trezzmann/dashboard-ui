4fa8e1bf1d7a6bca6d08bb42435ab316ee7f3e0d
Trezzmann <anahimana3@gmail.com>
2023-11-06 21:57:29 +0200

    dash done

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -10,6 +10,13 @@ import {
     Stack,
     Text,
     VStack,
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+    TableContainer,
 } from "@chakra-ui/react";
 import { BsCalendar4Event, BsPatchCheckFill } from "react-icons/bs";
 import { LineChart } from "../charts/LineChart";
@@ -17,22 +24,26 @@ import { LineChart2 } from "../charts/LineChart2";
 import { DonutChart } from "../charts/DonutChart";
 import { DonutChart2 } from "../charts/DonutChart2";
 import { IoCheckmarkDoneSharp } from "react-icons/io5";
+import { BiDownArrowAlt, BiUpArrowAlt } from "react-icons/bi";
 
 export const Dashboard = () => {
     return (
         <Flex px={16} py={6} gap={4} wrap="wrap">
             {/* filters  */}
             <Stack justify="space-between" direction="row" w="full">
-                <HStack spacing={4}>
-                    <Select placeholder="" colorScheme="green" bg="#fff">
-                        <option value="option1">AGENT-0002</option>
+                <HStack spacing={3}>
+                    <Select
+                        placeholder=""
+                        colorScheme="green"
+                        bg="#fff"
+                        minW={"180px"}
+                    >
+                        <option value="option1">AGENT-0001</option>
                         <option value="option2">AGENT-0003</option>
                         <option value="option3">AGENT-0004</option>
                     </Select>
                     <Select placeholder="" bg="#fff">
-                        <option value="option1">AGENT-0002</option>
-                        <option value="option2">AGENT-0003</option>
-                        <option value="option3">AGENT-0004</option>
+                        <option value="option1">Store: All</option>
                     </Select>
                 </HStack>
                 <Flex
@@ -170,7 +181,7 @@ export const Dashboard = () => {
                         <Text fontWeight="semibold">Terminals</Text>
                     </HStack>
                     <Text pl={"12"}>
-                        <Text fontWeight="bold" fontSize={"2xl"} as="span">
+                        <Text fontWeight="bold" fontSize={"3xl"} as="span">
                             8,032{" "}
                         </Text>
                         <Text color="#636363" as="span">
@@ -207,9 +218,156 @@ export const Dashboard = () => {
                 border="1px solid #e5e5e5"
                 w="auto"
                 flex={1}
-                h="60vh"
                 p={6}
-            ></Flex>
+                // h="fit-content"
+                direction={"column"}
+                gap={6}
+            >
+                <Flex justify={"space-between"} w="full" align={"center"}>
+                    <HStack>
+                        <Center
+                            w={10}
+                            h={10}
+                            borderRadius="full"
+                            bg="#F9F2EC"
+                            fontSize="xl"
+                            color="#E37E46"
+                        >
+                            <BsPatchCheckFill />
+                        </Center>
+                        <Text fontWeight="semibold">Leaderboard</Text>
+                    </HStack>
+                    <Select placeholder="" bg="#fff" w={"fit-content"}>
+                        <option value="option1">Agents</option>
+                        <option value="option2">Shora</option>
+                        <option value="option3">Sales</option>
+                    </Select>
+                </Flex>
+                <TableContainer>
+                    <Table variant="simple">
+                        <Thead bg="#F9F9F9">
+                            <Tr>
+                                <Th>Ranking</Th>
+                                <Th>Agent</Th>
+                                <Th>Transactions</Th>
+                                <Th>Completion Rate</Th>
+                                <Th isNumeric>Sales</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr
+                                _hover={{ bg: "#FBE4D4" }}
+                                transition="all ease 300ms"
+                            >
+                                <Td py={2}>
+                                    <HStack align={"center"}>
+                                        <Center
+                                            w={6}
+                                            h={6}
+                                            borderRadius={"full"}
+                                            bg={"gray.100"}
+                                            p={5}
+                                        >
+                                            80
+                                        </Center>
+                                        <Text color={"green.400"}>
+                                            <BiUpArrowAlt />
+                                            {/* <BiDownArrowAlt /> */}
+                                        </Text>
+                                    </HStack>
+                                </Td>
+                                <Td py={2}>AGENT-0080</Td>
+                                <Td py={2}>92</Td>
+                                <Td py={2}>81%</Td>
+                                <Td py={2} isNumeric>
+                                    <Text>
+                                        26,040{" "}
+                                        <Text
+                                            fontSize={"sm"}
+                                            color={"gray.500"}
+                                            as="span"
+                                        >
+                                            BIF
+                                        </Text>
+                                    </Text>
+                                </Td>
+                            </Tr>
+                            <Tr
+                                _hover={{ bg: "#FBE4D4" }}
+                                transition="all ease 300ms"
+                            >
+                                <Td py={2}>
+                                    <HStack align={"center"}>
+                                        <Center
+                                            w={6}
+                                            h={6}
+                                            borderRadius={"full"}
+                                            bg={"gray.100"}
+                                            p={5}
+                                        >
+                                            81
+                                        </Center>
+                                        <Text color={"red.400"}>
+                                            <BiDownArrowAlt />
+                                        </Text>
+                                    </HStack>
+                                </Td>
+                                <Td py={2}>AGENT-0464</Td>
+                                <Td py={2}>92</Td>
+                                <Td py={2}>91%</Td>
+                                <Td py={2} isNumeric>
+                                    <Text>
+                                        36,040{" "}
+                                        <Text
+                                            fontSize={"sm"}
+                                            color={"gray.500"}
+                                            as="span"
+                                        >
+                                            BIF
+                                        </Text>
+                                    </Text>
+                                </Td>
+                            </Tr>
+                            <Tr
+                                _hover={{ bg: "#FBE4D4" }}
+                                transition="all ease 300ms"
+                            >
+                                <Td py={2}>
+                                    <HStack align={"center"}>
+                                        <Center
+                                            w={6}
+                                            h={6}
+                                            borderRadius={"full"}
+                                            bg={"gray.100"}
+                                            p={5}
+                                        >
+                                            81
+                                        </Center>
+                                        <Text color={"red.400"}>
+                                            <BiDownArrowAlt />
+                                        </Text>
+                                    </HStack>
+                                </Td>
+                                <Td py={2}>AGENT-0464</Td>
+                                <Td py={2}>92</Td>
+                                <Td py={2}>91%</Td>
+                                <Td py={2} isNumeric>
+                                    <Text>
+                                        36,040{" "}
+                                        <Text
+                                            fontSize={"sm"}
+                                            color={"gray.500"}
+                                            as="span"
+                                        >
+                                            BIF
+                                        </Text>
+                                    </Text>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                </TableContainer>
+            </Flex>
         </Flex>
     );
 };
